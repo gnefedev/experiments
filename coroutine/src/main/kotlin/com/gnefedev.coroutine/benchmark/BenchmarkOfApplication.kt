a8d04345ad8091f3494f231dad077315df013db7
Georgy Nefedev <gnefedev@yandex-team.ru>
2017-10-08 00:22:21 +0300

    benchmarks

--- coroutine/src/main/kotlin/com/gnefedev.coroutine/benchmark/BenchmarkOfApplication.kt
+++ coroutine/src/main/kotlin/com/gnefedev.coroutine/benchmark/BenchmarkOfApplication.kt
@@ -1,20 +1,24 @@
 package com.gnefedev.coroutine.benchmark
 
 import com.gnefedev.coroutine.helper.RandomHolder
-import org.openjdk.jmh.annotations.Benchmark
-import org.openjdk.jmh.annotations.Fork
-import org.openjdk.jmh.annotations.Threads
+import org.openjdk.jmh.annotations.*
 import org.openjdk.jmh.runner.Runner
 import org.openjdk.jmh.runner.options.OptionsBuilder
 import org.springframework.web.client.RestTemplate
 import kotlin.math.absoluteValue
 
-val httpClient = RestTemplate()
+private val httpClient = RestTemplate()
 
 //Benchmark                      Mode  Cnt    Score    Error  Units
 //BenchmarkOfApplication.async  thrpt   20  151.128 ±  8.315  ops/s
 //BenchmarkOfApplication.sync   thrpt   20  157.127 ± 10.344  ops/s
+//Benchmark                          Mode  Cnt   Score   Error  Units
+//BenchmarkOfApplication.httpAsync  thrpt   20  21.010 ± 0.350  ops/s
+//BenchmarkOfApplication.httpSync   thrpt   20  20.493 ± 0.215  ops/s
+//BenchmarkOfApplication.httpAsync   avgt   20   0.193 ± 0.005   s/op
+//BenchmarkOfApplication.httpSync    avgt   20   0.198 ± 0.002   s/op
 @Fork(1)
+@BenchmarkMode(Mode.AverageTime, Mode.Throughput)
 @Threads(Threads.MAX)
 class BenchmarkOfApplication {
     @Benchmark
@@ -25,6 +29,17 @@ class BenchmarkOfApplication {
     @Benchmark
     fun async(randomHolder: RandomHolder) = httpClient.getForEntity(
             "http://localhost:8080/async/${randomHolder.random.nextInt(10_000).absoluteValue}",
+            String::class.java
+    )
+
+    @Benchmark
+    fun httpSync() = httpClient.getForEntity(
+            "http://192.168.0.11:8080/http/sync/100/1",
+            String::class.java
+    )
+    @Benchmark
+    fun httpAsync() = httpClient.getForEntity(
+            "http://192.168.0.11:8080/http/async/100/1",
             String::class.java
     )
 }
--- coroutine/src/main/kotlin/com/gnefedev.coroutine/benchmark/BenchmarkOfHttp.kt
+++ coroutine/src/main/kotlin/com/gnefedev.coroutine/benchmark/BenchmarkOfHttp.kt
@@ -9,10 +9,15 @@ import org.openjdk.jmh.annotations.Fork
 import org.openjdk.jmh.annotations.Threads
 import org.openjdk.jmh.runner.Runner
 import org.openjdk.jmh.runner.options.OptionsBuilder
+import org.springframework.web.client.RestTemplate
 
-//Benchmark                              Mode  Cnt   Score   Error  Units
-//BenchmarkOfHttp.asyncReadInCoroutine  thrpt   20  98.375 ± 5.721  ops/s
-//BenchmarkOfHttp.syncReadInCoroutine   thrpt   20  90.700 ± 3.713  ops/s
+private val httpClient = RestTemplate()
+
+
+//Benchmark                              Mode  Cnt    Score   Error  Units
+//BenchmarkOfHttp.asyncReadInCoroutine  thrpt   20  304.831 ± 8.281  ops/s
+//BenchmarkOfHttp.restTemplate          thrpt   20  302.579 ± 7.834  ops/s
+//BenchmarkOfHttp.syncReadInCoroutine   thrpt   20  307.901 ± 8.304  ops/s
 @Fork(1)
 @Threads(Threads.MAX)
 class BenchmarkOfHttp {
@@ -25,14 +30,20 @@ class BenchmarkOfHttp {
         httpGetAsync(asyncClientHolder.client)
     }
 
+    @Benchmark
+    fun restTemplate(): String = httpClient.getForEntity(
+            "http://192.168.0.11:8080/stub/10",
+            String::class.java
+    ).body
+
     fun httpGetSync(asyncHttpClient: AsyncHttpClient): String = asyncHttpClient
-            .prepareGet("http://www.ya.ru/")
+            .prepareGet("http://192.168.0.11:8080/stub/10")
             .execute()
             .get()
             .responseBody
 
     suspend fun httpGetAsync(asyncHttpClient: AsyncHttpClient): String = asyncHttpClient
-            .prepareGet("http://www.ya.ru/")
+            .prepareGet("http://192.168.0.11:8080/stub/10")
             .executeAsync()
             .responseBody
 }
